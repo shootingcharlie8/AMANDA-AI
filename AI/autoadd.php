e0467d07d971c3e7371a0f205fb7d908d8f8006f
shootingcharlie8 <imagemaster77@gmail.com>
2016-03-07 21:08:40 -0600

    autoadd

---
+++ AI/autoadd.php
@@ -0,0 +1,32 @@
+<?php
+   if ($_POST) {
+      $keyword = $_POST['keyword'];
+      $response = $_POST['response'];
+      $keywordupper = strtoupper($keyword);
+      $responseupper = strtoupper($response);
+      $keywordFinal = "K" . $keywordupper . "\n";
+      $responseFinal = "K" . $responseupper . "\n";
+      if( strpos(file_get_contents("user.txt"),$keywordFinal) !== false) {
+         echo "Error: Keyword Exists!!";
+         // do stuff
+      }
+      else {
+         file_put_contents("user.txt", $keywordFinal, FILE_APPEND);
+         file_put_contents("user.txt", $responseFinal, FILE_APPEND);
+         file_put_contents("user.txt", "#\n", FILE_APPEND);
+         echo "Success!";
+      }
+   }
+
+?>
+<html>
+   <body>
+      <form action="" method="post">
+         Keyword: <input type="text" name="keyword" value="keyword" />
+         Response: <input type="text" name="response" value="response" />
+         <input type = "submit" />
+      </form>
+      
+   
+   </body>
+</html>
