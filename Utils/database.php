8708fb7726380ebb316ab7f048a54f86c1e03023
Charlie Melidosian <gitlab@melidosianjr.com>
2016-08-28 01:53:20 -0500

    Added comments

--- Utils/database.php
+++ Utils/database.php
@@ -17,44 +17,44 @@ if (isset($_GET['keyword'])) {
     PDO::ATTR_EMULATE_PREPARES   => false,
   ];
   $pdo = new PDO($dsn, $user, $pass, $opt);
-
+  // Counting Varibles (Some are not needed, I know.)
   $p = 0;
   $o = 0;
   $s = 0;
   $simarray = array();
   $simarray2 = array();
   $simarray3 = array();
+  // Puts input to UPPERCASE for easier parsing
   $keyword3 = strtoupper($_GET['keyword']);
-
+  // Seperates every word
   $responses2 = explode(" ", $keyword3);
+  // Lists common words to remove
   $removedWords = array('OF', 'IN', 'TO', 'FOR', 'WITH', 'ON', 'AT', 'FROM', 'BY', 'AS');
+  // Removes common words
   $responses = array_diff($responses2, $removedWords);
   $a = 0;
+  // Call database for response
   $stmt = $pdo->prepare ("SELECT *, LEVENSHTEIN(`Keyword`, :keyword) AS distance FROM amanda3 ORDER BY `distance` ASC LIMIT 1");
   $stmt->execute(['keyword' => $keyword3]);
   $result = $stmt->fetch();
   $i = 0;
+  // ****IMPORTANT****
+  //WITHOUT THIS ECHO, THE SCRIPT WOULD NOT BE ABLE TO REPLY
   echo ($result['Response']);
-  if (sizeof($result) > 7) {
-
-}
-else {
-}
-$a++;
-
-function sort_by_order ($a, $b)
-{
-  return $b['sim'] -  $a['sim'];
-}
-$d = 0;
-while ($o < count($simarray)) {
-
-  while ($simarray[$p]['sim'] === $simarray[0]['sim']) {
-    $p++;
+  $a++;
+  
+  function sort_by_order ($a, $b)
+  {
+    return $b['sim'] -  $a['sim'];
   }
-  $o++;
-}
-$s = $p-1;
+  while ($o < count($simarray)) {
+
+    while ($simarray[$p]['sim'] === $simarray[0]['sim']) {
+      $p++;
+    }
+    $o++;
+  }
+  $s = $p-1;
 
 }
 ?>
