6a6528e39ee0f95a2e37c147ad9536eb965812f9
Charlie Melidosian <gitlab@melidosianjr.com>
2016-08-28 02:05:04 -0500

    Commented main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
+// Gets input from index.php
 function getuserinput() {
-    var inputField = document.getElementById('userresponse').value;
-    var userinput2 = inputField.toUpperCase();
-    var punctuationless = userinput2.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"");
-    var userinput = punctuationless.replace(/\s{2,}/g," ");
-    inputField = "";
-    return userinput;
+  var inputField = document.getElementById('userresponse').value;
+  // Makes input UPPERCASE
+  var userinput2 = inputField.toUpperCase();
+  // Replaces special charicters
+  var punctuationless = userinput2.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"");
+  var userinput = punctuationless.replace(/\s{2,}/g," ");
+  inputField = "";
+  return userinput;
 }
 function inputtext() {
-    var learning = false;
-    var userinput = getuserinput();
-    var div = document.getElementById('response');
-    var punctuationless = userinput.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"");
-    var userinput = punctuationless.replace(/\s{2,}/g," ");
-    if (userinput != "" && learning == false) {
-        div.innerHTML = div.innerHTML + '<p>YOU: ' + userinput + '</p>';
-        getresponse(userinput, div);
-        var inputField = document.getElementById('userresponse');
-        inputField.value = "";
-    }
+  // Calls getuserinput() and sets returned value to userinput
+  var userinput = getuserinput();
+  var div = document.getElementById('response');
+  //More stripping of special charicters (Its redundant)
+  var punctuationless = userinput.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"");
+  var userinput = punctuationless.replace(/\s{2,}/g," ");
+  if (userinput != "" && learning == false) {
+    // Returns what you typed
+    div.innerHTML = div.innerHTML + '<p>YOU: ' + userinput + '</p>';
+    // Calls the getresponse function
+    getresponse(userinput, div);
+    // Resets input box
+    var inputField = document.getElementById('userresponse');
+    inputField.value = "";
+  }
 }
 function getresponse(userinput, div){
-    $.get( "Utils/database.php", { keyword: userinput} )
-    .done(function( data ) {
-        data2 = data.toString();
-        div.innerHTML = div.innerHTML + '<p>AMANDA: ' + data2.replace(/(\r\n|\n|\r)/gm,"") + '</p>';
-        div.scrollTop = div.scrollHeight;
-        $.get( "Utils/log.php", { keyword: userinput, response: data2.replace(/(\r\n|\n|\r)/gm,"") } );
-    });
+  // Sends GET command to Utils/database.php to get a response
+  $.get( "Utils/database.php", { keyword: userinput} )
+  .done(function( data ) {
+    // Converts the returned response to String
+    data2 = data.toString();
+    // Strips line endings and prints response
+    div.innerHTML = div.innerHTML + '<p>AMANDA: ' + data2.replace(/(\r\n|\n|\r)/gm,"") + '</p>';
+    // Scroll down text box
+    div.scrollTop = div.scrollHeight;
+    // Logs everything!
+    $.get( "Utils/log.php", { keyword: userinput, response: data2.replace(/(\r\n|\n|\r)/gm,"") } );
+  });
 }
